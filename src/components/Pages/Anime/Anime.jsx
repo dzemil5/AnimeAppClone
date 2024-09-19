990b89ac3cae5903a8ec8df7ef5381864ca841be
dzemil5 <dzemil.sahovic@gmail.com>
2024-09-19 19:18:58 +0200

    Izmenjen los CSS

--- src/components/Pages/Anime/Anime.jsx
+++ src/components/Pages/Anime/Anime.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FetchAnime from "../../FetchAnime/FetchAnime";
 import SearchBar from "../../Search Bar/searchBar";
 import CategorySearch from "../../CategorySearch/categorySearch";
 
 const Anime = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
 
   return (
     <div>
--- src/components/Pages/Manga/Manga.jsx
+++ src/components/Pages/Manga/Manga.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FetchManga from "../../FetchManga/FetchManga";
 import SearchBar from "../../Search Bar/searchBar";
 import CategorySearch from "../../CategorySearch/categorySearch";
 
 const Manga = () => {
-
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+  
   return (
     <div>
       <CategorySearch />
